be9faf9070410b8023448031f5c81bf4a2eb85aa
SametK <samet_kara55@hotmail.com>
2022-07-08 16:34:13 +0300

    runner update

--- src/test/java/com/xfleet/runners/Runner.java
+++ src/test/java/com/xfleet/runners/Runner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         glue = "com/xfleet/step_definitions",
         publish = true,
         dryRun = false,
-        tags = "@FLTAPS-1884 or @FLTAPS-1885 or @FLTAPS-1887"
+        tags = ""
 )
 public class Runner {
 }
